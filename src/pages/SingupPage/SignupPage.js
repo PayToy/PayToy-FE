83b7cacfc7463a552730c128c99feb2d989b94fa
RohSungKyun <songkyun1108@naver.com>
2024-11-17 20:13:30 +0900

    feat : 유효성 검사 로직 추가

--- src/pages/SingupPage/SignupPage.js
+++ src/pages/SingupPage/SignupPage.js
@@ -2,14 +2,17 @@ import { useState } from "react";
 import AuthInput from "../../components/AuthInput/AuthInput.js";
 import { Title } from "./style.js";
 import { Wrapper } from "../LoginPage/style.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthButton from "../../components/AuthButton/AuthButton.js";
+import { validateUser } from "../../utils/validator.js";
 
 const SignupPage = () => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     username: '',
     tel: '',
     password: '',
+    passwordConfirm: '',
   })
   const handleData = (e) => {
     const {name, value} = e.target
@@ -19,12 +22,22 @@ const SignupPage = () => {
       [name] : value,
     })
   }
+  const handleSubmit = () => {
+    const error = validateUser(formData);
+    if(Object.keys(error).length > 0){
+      alert(Object.values(error).join('\n'));
+    } else {
+      // 회원가입 추가 로직
+      navigate('/');
+      alert("회원가입 성공!");
+    }
+  }
 
   return (
     <Wrapper>
       <Title>PayToy</Title>
       <AuthInput
-        name="name"
+        name="username"
         placeholder="name"
         type="text"
         value={formData.username}
@@ -48,10 +61,10 @@ const SignupPage = () => {
         name="passwordConfirm"
         placeholder="Re-enter password"
         type="Password"
-        value={formData.username}
+        value={formData.passwordConfirm}
         onChange={handleData}
       />
-      <AuthButton title="회원가입" />
+      <AuthButton title="회원가입" onClick={handleSubmit}/>
       <Link to={"/"}>로그인</Link>
     </Wrapper>
 
