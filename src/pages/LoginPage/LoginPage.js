6f0e21efdd257c29e3ff7affe9fadad25b75813f
RohSungKyun <songkyun1108@naver.com>
2024-11-18 18:53:16 +0900

    feat : 로그인 api 연동

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,20 +1,37 @@
 import { useState } from "react";
 import AuthInput from "../../components/AuthInput/AuthInput.js";
 import { Title, Wrapper } from "./style.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthButton from "../../components/AuthButton/AuthButton.js";
+import { userLogin } from "../../api/AuthAPI.js";
 
 
 const LoginPage = () => {
   const [tel, setTel] = useState("");
   const [password, setPassword] = useState("");
-
+  const navigate = useNavigate();
   const handleData = (e) => {
     if (e.target.name === "tel") {
       setTel(e.target.value);
     }
     if (e.target.name === "password") {
       setPassword(e.target.value);
+    }
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const response = await userLogin({tel, password});
+      if (response.status === 201) {
+        // 성공 시 세션id 저장..? 로컬스토리지
+        alert("로그인 성공");
+        navigate('/main');
+      } else {
+        alert("로그인 실패");
+      }
+    } catch (error) {
+      alert("로그인 중 오류 발생");
+      console.log(error);
     }
   }
 
@@ -37,7 +54,7 @@ const LoginPage = () => {
         onChange={handleData}
         required
       />
-      <AuthButton title = "로그인"/>
+      <AuthButton title = "로그인" onClick={handleSubmit}/>
       <Link to={"/signup"}>회원가입</Link>
 
     </Wrapper>
